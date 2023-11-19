f2d9f3e8a29757889b6ee864c91d0931016a941c
Tyuiu.ZargarovAA@outlook.com <fairmarko@gmail.com>
2023-11-19 21:33:22 +0300

    Task3

--- Tyuiu.ZargarovAA.Sprint5.Task3.V7.Test/DataServiceTest.cs
+++ Tyuiu.ZargarovAA.Sprint5.Task3.V7.Test/DataServiceTest.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+using System.IO;
+using Tyuiu.ZargarovAA.Sprint5.Task3.V7.Lib;
 
 namespace Tyuiu.ZargarovAA.Sprint5.Task3.V7.Test
 {
@@ -7,8 +8,15 @@ namespace Tyuiu.ZargarovAA.Sprint5.Task3.V7.Test
     public class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Task3()
         {
+            string path = @"C:\Users\fairm\OneDrive\Рабочий стол\source\repos\Tyuiu.ZargarovAA.Sprint5\Tyuiu.ZargarovAA.Sprint5.Task3.V7\bin\Debug\OutPutFileTask3.bin";
+
+            FileInfo fileInfo = new FileInfo(path);
+
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
         }
     }
 }
